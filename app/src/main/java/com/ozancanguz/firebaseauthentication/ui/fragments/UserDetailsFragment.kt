00f80dea9701fe413970d5a36102cb93c644820c
Ozan <ozancangu24@gmail.com>
2022-12-24 13:52:23 +0300

    -sign out menu and function created

--- app/src/main/java/com/ozancanguz/firebaseauthentication/ui/fragments/UserDetailsFragment.kt
+++ app/src/main/java/com/ozancanguz/firebaseauthentication/ui/fragments/UserDetailsFragment.kt
@@ -1,10 +1,13 @@
 package com.ozancanguz.firebaseauthentication.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozancanguz.firebaseauthentication.R
 import com.ozancanguz.firebaseauthentication.databinding.FragmentLoginBinding
 import com.ozancanguz.firebaseauthentication.databinding.FragmentUserDetailsBinding
@@ -12,8 +15,9 @@ import com.ozancanguz.firebaseauthentication.databinding.FragmentUserDetailsBind
 
 class UserDetailsFragment : Fragment() {
     private var _binding: FragmentUserDetailsBinding? = null
-
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +28,31 @@ class UserDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         _binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
         val view = binding.root
+        FirebaseApp.initializeApp(requireContext())
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        setHasOptionsMenu(true)
+
 
 
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.signoutmenu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.signOutMenu){
+            findNavController().navigate(R.id.action_userDetailsFragment_to_loginFragment)
+            auth.signOut()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
